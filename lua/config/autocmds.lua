fa70d704ceaaf4b50e8963903cc315ebf4c45717
timeknow01 <54026498+timeknow01@users.noreply.github.com>
2025-08-03 13:21:38 +0800

    first commit

---
+++ lua/config/autocmds.lua
@@ -0,0 +1,24 @@
+-- Turn off paste mode when leaving insert
+vim.api.nvim_create_autocmd("InsertLeave", {
+	pattern = "*",
+	command = "set nopaste",
+})
+
+-- Disable the concealing in some file formats
+-- The default conceallevel is 3 in LazyVim
+--
+--
+
+vim.api.nvim_create_autocmd("FileType", {
+	pattern = { "markdown", "txt" },
+	callback = function()
+		vim.opt_local.spell = false
+	end,
+})
+
+vim.api.nvim_create_autocmd("FileType", {
+	pattern = { "json", "jsonc", "markdown" },
+	callback = function()
+		vim.opt.conceallevel = 0
+	end,
+})
